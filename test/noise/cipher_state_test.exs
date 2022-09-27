bb2651a5bdb82e303eef7c6b8c223e42df878fd7
Sgiath <sgiath@sgiath.dev>
2022-09-27 09:50:58 +0200

    test: add some tests
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

---
+++ test/noise/cipher_state_test.exs
@@ -0,0 +1,5 @@
+defmodule NoiseTest.CipherState do
+  use ExUnit.Case, async: true
+
+  doctest Noise.CipherState
+end
---
+++ test/noise/crypto/dh/secp256k1_test.exs
@@ -0,0 +1,7 @@
+defmodule NoiseTest.Crypto.DH.Secp256k1 do
+  use ExUnit.Case, async: true
+  
+  doctest Noise.Crypto.DH.Secp256k1
+  
+  test
+end
